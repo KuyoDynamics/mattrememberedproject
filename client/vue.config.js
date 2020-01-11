9b9ba07889f8c1ffdcb506e193339c2ae8fb1441
Chaiwa Berian <chaiwa.berian@bantulabtech.com>
2020-01-11 16:56:12 +0200

    Edit Webpack config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  publicPath: process.env.NODE_ENV === "production" ? "/../public/" : "/",
+  outputDir: process.env.NODE_ENV === "production" ? "/../public/dist/" : "dist"
 };
+
+// customize it so when you want to test as a standalone vuejs,
+// run build(default) that builds to dist
+// but when you want it integrated with the server
+// run build(with custom config that builds to server's /public folder)
